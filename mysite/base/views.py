eb099a4f51baaef866d5b3a5c5886a5667f3f9f2
JAGDISH0008 <jagadeesh26597@gmail.com>
2022-04-17 15:52:49 +0530

    firebase admin added and token generated.

--- mysite/base/views.py
+++ mysite/base/views.py
@@ -1,9 +1,17 @@
 from django.shortcuts import render
 from .models import User
 from django.contrib.auth.hashers import check_password
+import firebase_admin
+from firebase_admin import credentials, auth
+
+
+cred = credentials.Certificate("django-firebase-auth-cred.json")
+admin = firebase_admin.initialize_app(cred)
+
 
 
 # Create your views here.
+
 
 def index(request):
     return render(request, 'base/index.html')
@@ -22,13 +30,17 @@ def postsignIn(request):
         # user=authe.sign_in_with_email_and_password(email,pasw)
         user = User.objects.get(email=email,tenant=tenant)
         matchcheck= check_password(pasw,user.password)
-        # if matchcheck:
+        custom_claims = {'id': user.id, 'tenant': user.tenant,'role':"ADMIN"}
+        if matchcheck:
+          token = auth.create_custom_token('b27e7408-be32-11ec-9d64-0242ac120002', custom_claims)
+          print(token)
 
         
         
-    except:
-        message="Invalid Credentials!!Please ChecK your Data"
-        return render(request,"base/login.html",{"message":message})
+    except Exception as e:
+      print(e)
+      message="Invalid Credentials!!Please ChecK your Data"
+      return render(request,"base/login.html",{"message":message})
     # session_id=user['idToken']
     # request.session['uid']=str(session_id)
     return render(request,"base/home.html",{"email":email})
